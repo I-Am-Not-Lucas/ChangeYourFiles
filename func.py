23b49ac1aa5d5a9a567a75d3a4299726a6ca2a1e
I-Am-Not-Lucas <0lucasa2000@hotmail.com.br>
2022-10-26 11:13:04 -0300

    capitalize function

--- func.py
+++ func.py
@@ -1,18 +1,42 @@
 import os
 import shutil 
 
-def capitalize():
-    pass
-
 def path_test(caminho):
     try:
-        os.mkdir(caminho)
+        directory = "teste"
+        parent_dir = caminho
+        path = os.path.join( parent_dir, directory)
+        os.mkdir(path)
 
-    except Exception as e:
-        print("Invalid path")
-        print(e)
+    except FileExistsError:
+        print("Caminho validado, teste já existente")
+
+    except PermissionError:
+        print("Sem permissão :(")
+
+    except:
+        print("Caminho inválido ou inexistente")
+       
 
     else:
         print(f"caminho {caminho} é válido")
+
+
+def trata_caminho(raw_path):
+    caminho_tratado = raw_path.replace("\\", "\\\\")
+
+    return caminho_tratado
+
+
+def capitalize(caminho):
+    for root, dirs, files in os.walk(caminho):
+        for file in files:
+            name, ext = os.path.splitext(file)
+            new_name = name.capitalize() + ext
+            print(file, new_name)
+            os.rename(files, new_name)
+            
+
+
 
 
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-
 from func import *
 
 
-print("Choose a option")
-print("[1] Captilize")
+# print("Choose a option")
+# print()
+# print("[1] Captilize")
 
-caminho = ("C:\Programacao\Python\PROJETOS\ChangeYourFiles\pasta_testes")
-caminho = caminho.replace("\\", "\\\\")
-path_test(caminho)
+variavel_caminho = r"C:\Programacao\Python\PROJETOS\ChangeYourFiles\testeCaptilze"
+caminho = trata_caminho(variavel_caminho)
+# path_test(caminho)
 
 escolha = '1'
 
 
 while True:
     if escolha == '1':
-        capitalize()
+        capitalize(caminho)
         break
+
     else:
         print("opção inválida")
         break
